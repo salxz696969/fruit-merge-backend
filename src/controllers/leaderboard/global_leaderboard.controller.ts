ae4de94d63dcceeab16f25e9c37ca97b65d65d67
Chanraksa Ory <OCraksa@gmail.com>
2025-12-16 14:48:25 +0700

    changed global leaderboard to only get name and score

--- src/controllers/leaderboard/global_leaderboard.controller.ts
+++ src/controllers/leaderboard/global_leaderboard.controller.ts
@@ -2,43 +2,61 @@ import { Request, Response } from "express";
 import { UserSubmission } from "../../mongoose/user_submission.model";
 
 export const globalTodaysLeaderboard = async (_req: Request, res: Response) => {
-	try {
-		const startOfDay = new Date();
-		startOfDay.setHours(0, 0, 0, 0);
+  try {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
 
-		const submissions = await UserSubmission.find({ date: { $gte: startOfDay } })
-			.sort({ score: -1 })
-			.limit(10)
-			.exec();
+    const submissions = await UserSubmission.find({
+      date: { $gte: startOfDay },
+    })
+      .sort({ score: -1 })
+      .limit(10)
+      .exec();
 
-		return res.status(200).json(submissions);
-	} catch (error) {
-		return res.status(500).send("Error retrieving today's global leaderboard");
-	}
+    return res.status(200).json(submissions);
+  } catch (error) {
+    return res.status(500).send("Error retrieving today's global leaderboard");
+  }
 };
 
-export const globalThisWeekLeaderboard = async (_req: Request, res: Response) => {
-	try {
-		const sevenDaysAgo = new Date();
-		sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+export const globalThisWeekLeaderboard = async (
+  _req: Request,
+  res: Response
+) => {
+  try {
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 
-		const submissions = await UserSubmission.find({ date: { $gte: sevenDaysAgo } })
-			.sort({ score: -1 })
-			.limit(10)
-			.exec();
+    const submissions = await UserSubmission.find({
+      date: { $gte: sevenDaysAgo },
+    })
+      .sort({ score: -1 })
+      .limit(10)
+      .exec();
 
-		return res.status(200).json(submissions);
-	} catch (error) {
-		return res.status(500).send("Error retrieving this week's global leaderboard");
-	}
+    return res.status(200).json(submissions);
+  } catch (error) {
+    return res
+      .status(500)
+      .send("Error retrieving this week's global leaderboard");
+  }
 };
 
-export const globalAllTimeLeaderboard = async (_req: Request, res: Response) => {
-	try {
-		const submissions = await UserSubmission.find().sort({ score: -1 }).limit(10).exec();
+export const globalAllTimeLeaderboard = async (
+  _req: Request,
+  res: Response
+) => {
+  try {
+    const submissions = await UserSubmission.find(
+      { name: { $ne: null } },
+      { _id: 0, name: 1, score: 1 }
+    )
+      .sort({ score: -1 })
+      .limit(10)
+      .exec();
 
-		return res.status(200).json(submissions);
-	} catch (error) {
-		return res.status(500).send("Error retrieving all-time global leaderboard");
-	}
+    return res.status(200).json(submissions);
+  } catch (error) {
+    return res.status(500).send("Error retrieving all-time global leaderboard");
+  }
 };
